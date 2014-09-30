45967c78e652f8109fd513b35f65d010a6b037ef
James Bray <jrbrayjr2006@gmail.com>
2014-09-30 17:43:52 -0500

    Added new network helper class with inner class to connect to networks on a new thread

---
+++ Fut5/src/com/fut5/helper/NetworkHelper.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.fut5.helper;
+
+import android.os.AsyncTask;
+
+/**
+ * @author james_r_bray
+ *
+ */
+public class NetworkHelper {
+
+	/**
+	 * 
+	 */
+	public NetworkHelper() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void connectToNetwork(String username, String password) {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @author james_r_bray
+	 *
+	 */
+	private class NetworkConnector extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+}
