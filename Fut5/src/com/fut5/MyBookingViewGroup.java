79e77b57949b3241556ba2430c682c4eb2c021c0
James Bray <jrbrayjr2006@gmail.com>
2015-01-07 23:01:58 -0600

    Add custom ViewGroup for my bookings

---
+++ Fut5/src/com/fut5/MyBookingViewGroup.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.fut5;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * @author james_r_bray
+ *
+ */
+public class MyBookingViewGroup extends ViewGroup {
+
+	/**
+	 * @param context
+	 */
+	public MyBookingViewGroup(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public MyBookingViewGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public MyBookingViewGroup(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.view.ViewGroup#onLayout(boolean, int, int, int, int)
+	 */
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
